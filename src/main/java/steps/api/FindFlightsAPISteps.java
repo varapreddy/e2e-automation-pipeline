dec5f52487915e8d8868a0bff1bde7261b1177ac
Varaprasada R Chinnappareddy/MAXIMUS <VaraprasadaRChinnappareddy@maximus.com>
2018-08-07 02:21:57 -0500

    reverting back

--- src/main/java/steps/api/FindFlightsAPISteps.java
+++ src/main/java/steps/api/FindFlightsAPISteps.java
@@ -7,6 +7,6 @@ import static net.serenitybdd.rest.SerenityRest.given;
 public class FindFlightsAPISteps {
     @Step
     public void findFlights() {
-    	given().parameters("fromPort", "Paris", "toPort", "Buenos Aires").when().post("http://blazedemo.com/reserve22.php");
+    	given().parameters("fromPort", "Paris", "toPort", "Buenos Aires").when().post("http://blazedemo.com/reserve.php");
     }
 }
